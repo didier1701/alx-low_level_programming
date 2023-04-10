3f5ffa68abc722300c30b86c5f1c56546643babb
IMANIRAHARI Didier <didier54054@gmail.com>
2023-04-10 06:18:24 -0700

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+/**
+ * _putchar - writes the characterc to stdout
+ * @c: The character to print
+ *
+ * Return: On succes1.
+ * On error, -1 is returned, and errno is set appropriately
+ */
+in_putchar(char c)
+{
+return(write(1, &c, 1));
+}
